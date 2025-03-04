0b3335afd98d4910d16959a3fcf22587b4dd9ce5
VKrieger <krieger.vi@gmail.com>
2025-03-04 16:20:52 +0700

    Setup app for render

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener("submit", (e) => {
   messageTwo.textContent = "";
   messageOne.textContent = "Loading....."
 
-  fetch("http://localhost:3001/weather?address=" + location).then(
+  fetch("/weather?address=" + location).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
